e5c1ab5f9f0d904208726c8098df6485fc573b17
jinusong <sugwer7464@gmail.com>
2019-07-01 20:58:42 +0900

    [Update] Make transrator

--- app/src/main/java/com/jinwoo/machine_learning_kit/MainActivity.kt
+++ app/src/main/java/com/jinwoo/machine_learning_kit/MainActivity.kt
@@ -2,6 +2,12 @@ package com.jinwoo.machine_learning_kit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.ml.naturallanguage.FirebaseNaturalLanguage
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslatorOptions
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +15,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val options = FirebaseTranslatorOptions.Builder()
+            .setSourceLanguage(FirebaseTranslateLanguage.EN)
+            .setTargetLanguage(FirebaseTranslateLanguage.KO)
+            .build()
+        val englishGermanTranslator = FirebaseNaturalLanguage.getInstance().getTranslator(options)
+
+        englishGermanTranslator.downloadModelIfNeeded()
+            .addOnSuccessListener { toast("모델 다운로드 실패") }
+            .addOnFailureListener { exception -> toast("모델 다운로드 실패") }
+
+
+        main_trans_btn.onClick {
+            englishGermanTranslator.translate(main_english_edit.text.toString())
+                .addOnSuccessListener { translatedText ->
+                    toast("번역 성공")
+                    main_trans_korean.text = translatedText
+                }
+                .addOnFailureListener { exception -> toast("번역 실패") }
+        }
     }
 }
